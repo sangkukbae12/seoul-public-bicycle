05f615fed6c22af52fcb41a360fd1de535d3e238
sangkukbea <sangkukbea@naver.com>
2020-05-04 10:54:16 +0900

    add map in app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useContext } from 'react';
+import Map from './components/Map';
+import Controls from './components/Controls';
+import { MapContext } from './context';
 import './App.css';
 
 function App() {
+  const { viewState, handleChangeViewState, places, radius } = useContext(
+    MapContext
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Map
+        width="100vw"
+        height="100vh"
+        viewState={viewState}
+        onViewStateChange={handleChangeViewState}
+        places={places}
+        radius={radius}
+      />
+      <Controls />
     </div>
   );
 }
