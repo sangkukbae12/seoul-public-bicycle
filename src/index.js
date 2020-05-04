6b7500cbeca24741f0836495434de5dedbb8cc4b
sangkukbea <sangkukbea@naver.com>
2020-05-04 10:52:16 +0900

    provide context in index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ContextProvider from './context';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ContextProvider>
+      <App />
+    </ContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
